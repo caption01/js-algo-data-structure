de863784e8b5236fc2c234e92f76023a09ec22a0
nutchapon.h <nutchapon@enres.com>
2020-11-29 21:48:38 +0700

    update

---
+++ 11-bubble-sort/index.js
@@ -0,0 +1,46 @@
+function numCompaer(num1, num2) {
+  return num1 - num2;
+}
+
+console.log([20, 4, 15, 6].sort(numCompaer));
+
+const swap = (arr, idx1, idx2) => {
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+};
+
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // swap
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+function bubbleSort2(arr) {
+  let noSwap;
+
+  for (let i = arr.length; i > 0; i--) {
+    noSwap = true;
+
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // swap
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwap = false;
+      }
+    }
+
+    if (noSwap) break;
+  }
+
+  return arr;
+}
